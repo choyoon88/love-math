58e58fd05bb63383f6a297452bd3ba6e3939d302
choyoon88 <azureluster@gmail.com>
2023-02-16 21:34:23 +0000

    add even listeners for documents loading and button clicks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,19 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+    // "let button of buttons" are more modern than "i = 0; i < buttons.length; i++"
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked Submit!");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`);
+            }
+        })
+    }
+})
+
 function runGame() {
 
 }
